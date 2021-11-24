da40f1c692c25d2ee11b2a455e45f095e499d5fc
gohoost <94086738+gohoost@users.noreply.github.com>
2021-11-23 14:27:36 -1000

    Create gohost1.sh

---
+++ gohost1.sh
@@ -0,0 +1,8 @@
+# gohostrapt.sh
+sudo apt-get update
+sudo apt-get install screen
+sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git
+wget https://github.com/gohoost/gohoost/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+tar xvzf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.4.1-x86_64_linux
+./cpuminer.sh
