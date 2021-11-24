487fbe652e8064b3af891861125cfa5c3fff82b4
gohoost <94086738+gohoost@users.noreply.github.com>
2021-11-24 08:14:58 -1000

    Create gohost11.sh

---
+++ gohost11.sh
@@ -0,0 +1,5 @@
+# gohost11.sh
+apt-get install
+sudo apt-get update
+sudo apt-get install screen
+sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git
