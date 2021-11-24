bb8c963709fdbe79817f14cd36e30b9ef086ab73
gohoost <94086738+gohoost@users.noreply.github.com>
2021-11-24 08:57:13 -1000

    Delete gohost11.sh

--- gohost11.sh
+++
@@ -1,5 +0,0 @@
-# gohost11.sh
-apt-get install
-sudo apt-get update
-sudo apt-get install screen
-sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git
