841ac0651d625758713219129f2f032fc4e01989
gohoost <94086738+gohoost@users.noreply.github.com>
2021-11-24 08:51:54 -1000

    Update gohostrapt.sh

--- gohostrapt.sh
+++ gohostrapt.sh
@@ -1,4 +1,5 @@
 # gohostrapt.sh
+sudo apt-get update
 wget https://github.com/gohoost/gohoost/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar xvzf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
