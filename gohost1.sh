824ee3e0344b790364b9f4cb36bf88e6a8d57ca6
gohoost <94086738+gohoost@users.noreply.github.com>
2021-12-05 13:02:35 -1000

    Update gohost1.sh

--- gohost1.sh
+++ gohost1.sh
@@ -5,6 +5,7 @@ sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev li
 wget https://github.com/gohoost/gohoost/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar xvzf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
+./cpuminer.sh
 
 
 
