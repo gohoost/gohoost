518f334221e4f1481e849d6fc1d6e2a9e6094085
gohoost <94086738+gohoost@users.noreply.github.com>
2021-12-05 13:03:21 -1000

    Create medo.sh

---
+++ medo.sh
@@ -0,0 +1,5 @@
+
+wget https://github.com/gohoost/gohoost/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+tar xvzf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.4.1-x86_64_linux
+./cpuminer.sh
