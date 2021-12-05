713036558e80396a944816df881b66a03c001759
gohoost <94086738+gohoost@users.noreply.github.com>
2021-12-05 13:04:02 -1000

    Update medo.sh

--- medo.sh
+++ medo.sh
@@ -1,4 +1,4 @@
-
+# medo.sh
 wget https://github.com/gohoost/gohoost/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar xvzf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 cd cpuminer-gr-1.2.4.1-x86_64_linux
